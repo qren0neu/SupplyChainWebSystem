49e1cf0a5205520b0db9b9a96a92db21d28d4dc0
Qi Ren <ren.qi1@northeastern.edu>
2024-07-13 16:52:47 -0400

    enable spring cloud eureka registration for the project

---
+++ supplychainserver/src/main/java/com/qiren/supplychainserver/SupplychainserverApplication.java
@@ -0,0 +1,13 @@
+package com.qiren.supplychainserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplychainserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplychainserverApplication.class, args);
+	}
+
+}
---
+++ supplychainserver/src/test/java/com/qiren/supplychainserver/SupplychainserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiren.supplychainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplychainserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
